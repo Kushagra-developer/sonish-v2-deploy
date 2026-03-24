118d3c62de6d8fdcb1e019be35fb40cc4800db2b
Kushagra Sachdeva <72493826+MaximusKS1@users.noreply.github.com>
2026-03-24 22:39:40 +0530

    fix: remove duplicate generateToken import in userController

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -2,7 +2,6 @@ import User from '../models/userModel.js';
 import Otp from '../models/otpModel.js';
 import generateToken from '../utils/generateToken.js';
 import nodemailer from 'nodemailer';
-import generateToken from '../utils/generateToken.js';
 
 // @desc    Auth user & get token
 // @route   POST /api/users/login
